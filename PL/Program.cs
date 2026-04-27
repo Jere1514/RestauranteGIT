85a8ee04dea9810fb881158c5f20c06d2d9d118b
anajim02 <anajimenezd02@hotmail.com>
2026-04-27 16:14:56 -0600

    ft: se cambia mi cadena de conexion de mi capa DL a mi capa PL en el archivo program.cs Configuracion Finalizada y funcional

--- PL/Program.cs
+++ PL/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<DL.RestauranteContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Restaurante")));
 
+//builder.Services.AddScoped<BL.Restaurante>();
 
 var app = builder.Build();
 
