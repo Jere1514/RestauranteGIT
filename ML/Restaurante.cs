c611280c301d0a39838c7534fd63059781e7bec4
anajim02 <anajimenezd02@hotmail.com>
2026-04-27 16:29:15 -0600

    ft: Se agregan las clases: Result, Restaurante, Direccion, Colonia, Municipio y Estado en mi capa ML

---
+++ ML/Restaurante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Restaurante
+    {
+
+        public int IdRestaurante { get; set; }
+        public string Nombre { get; set; }
+        public byte[] Logo { get; set; }
+        public DateTime FechaApertura { get; set; }
+        public DateTime FechaCierre { get; set; }
+
+        public ML.Direccion Direccion { get; set; }
+        public List<object> Objects { get; set; }
+
+
+    }
+}
